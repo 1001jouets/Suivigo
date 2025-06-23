3fee046e07d2e727242dc55a97829182c633c35b
1001jouets <annabelle@1001jouets.fr>
2025-06-24 00:01:52 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 # backend/app.py
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/track", methods=["GET"])
 def track():
