e246be482335fb5c5c56d3b67324938b69af5981
1001jouets <annabelle@1001jouets.fr>
2025-06-24 00:03:59 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -106,3 +106,9 @@ def track():
             return jsonify({"error": "FedEx tracking failed", "details": str(e)}), 500
 
     return jsonify({"transporteur": transporteur, "tracking": num, "message": "Tracking non encore pris en charge pour ce transporteur"})
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
